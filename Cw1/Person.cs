4c1bf8a66e325fe8f6a4f5a4f1141704d2280516
Adrian Łada <s15157@pjwstk.edu.pl>
2020-03-01 16:23:59 +0100

    init

---
+++ Cw1/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Cw1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+    }
+}
---
+++ Cw1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //var newPerson = new Person { FirstName = "Adrian" };
+
+            //var tmp10 = 1;
+
+
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+
+
+                //2xx - chcemy
+                //4xx - nie odpowiada
+                //5xx - zle
+                if (response.IsSuccessStatusCode)
+                {
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(htmlContent);
+
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+
+                }
+            }        
+
+        }
+    }
+}
